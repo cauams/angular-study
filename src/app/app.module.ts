fdbe3dfa16d2cda997e7151dcf8c0712597d38e9
cauams <cauanzitobnu@gmail.com>
2023-09-16 20:57:13 -0300

    added the new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,18 @@ import { MaterialModule } from './material.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { CategoryComponent } from './category/category.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
+    HomeComponent,
+    PageNotFoundComponent,
+    CategoryComponent,
   ],
   imports: [
     BrowserModule,
